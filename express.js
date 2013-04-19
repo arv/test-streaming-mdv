143e5fa7dad279c5e36927458342d8b72d71dc25
Erik Arvidsson <arv@chromium.org>
2013-04-18 22:01:40 -0400

    Initial commit

---
+++ express.js
@@ -0,0 +1,49 @@
+'use strict';
+
+var express = require("express");
+var app = express();
+
+app.configure(function(){
+  app.use(express.static(__dirname));
+});
+
+// app.get('/', function(req, res){
+//   res.sendfile(__dirname + '/index.html');
+// });
+
+app.get('/feed.json', function(req, res) {
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+    'Transfer-Encoding': 'chunked'
+  });
+
+  var start = Date.now();
+  var counter = 0;
+
+  res.write(Array(1000).join(' '));
+
+  res.write('[');
+
+  function writeMore() {
+    if (counter > 100) {
+      clearTimeout(t);
+      res.end(']');
+      return;
+    }
+
+    if (counter++ > 0)
+      res.write(',');
+
+    var obj = {
+      counter: counter,
+      now: Date.now()
+    };
+
+    res.write(JSON.stringify(obj));
+  }
+
+  var t = setInterval(writeMore, 500);
+
+});
+
+app.listen(3000);
